974f89ffcd614ad65c1e1a7ed5fed7b0e0c34222
harsh2204 <harshgupta2204@live.com>
2017-06-07 19:39:01 -0400

    Updated the css and added the buttons

--- sketch.js
+++ sketch.js
@@ -4,24 +4,30 @@ function setup() {
   noCanvas();
   var timer = new Timer();
   timer.createTimer(10);
-  // timer.startTimer();
-  //NEED BUTTON
-  // slate.attribute("lang","latex");
-  // var s = "\\frac{1+sin(x)}{y}";
-  // slate.html(s);
   createSlate();
   setupEquation();
-
-
-
+  makeButton();
 }
 function createSlate(){
   slate = createDiv("");
   slate.id("slate");
 }
 function setupEquation(){
+  // var s = "\\frac{1+sin(x)}{y}";
   var equation = createImg(callLink+"\e^{i\\pi}+1");
   equation.parent(slate);
   $('#slate').children("img").hide();
   $('#slate').children("img").fadeIn();
+}
+
+function makeButton(){
+  var buttonDiv = createDiv("");
+  var start = createButton("Start Timer");
+  var newEQ = createButton("New Equation");
+  start.parent(buttonDiv);
+  newEQ.parent(buttonDiv);
+  buttonDiv.id("butDiv");
+  start.id('start');
+  newEQ.id('start');
+  start.mousePressed(timer.startTimer());
 }
