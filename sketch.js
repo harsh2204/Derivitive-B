575c8f98b769de3d9320a1b3a03a056aff5440a4
harsh2204 <harshgupta2204@live.com>
2017-06-11 23:35:19 -0400

    Updated with a sample json

--- sketch.js
+++ sketch.js
@@ -1,39 +1,74 @@
 var callLink = "http://latex.codecogs.com/svg.latex?";
 var slate;
 var start;
+var problemSet;
+var difficulty;
+
 function setup() {
-  noCanvas();
-  var timekeeper = new Timer();
-  timekeeper.createTimer(10);
-  createSlate();
-  setupEquation();
-  makeButton();
-  
+    noCanvas();
+    var timekeeper = new Timer();
+    timekeeper.createTimer(10);
+    createSlate();
+    setupSelector();
+    setupEquation();
+    makeButton();
 
-  start.mousePressed(function(){
-    timekeeper.startTimer()
-  });
+    start.mousePressed(function() {
+        timekeeper.startTimer()
+    });
 
 }
-function createSlate(){
-  slate = createDiv("");
-  slate.id("slate");
-}
-function setupEquation(){
-  // var s = "\\frac{1+sin(x)}{y}";
-  var equation = createImg(callLink+"\e^{i\\pi}+1");
-  equation.parent(slate);
-  $('#slate').children("img").hide();
-  $('#slate').children("img").fadeIn();
+
+function createSlate() {
+    slate = createDiv("");
+    slate.id("slate");
 }
 
-function makeButton(){
-  var buttonDiv = createDiv("");
-  start = createButton("Start Timer");
-  var newEQ = createButton("New Equation");
-  start.parent(buttonDiv);
-  newEQ.parent(buttonDiv);
-  buttonDiv.id("butDiv");
-  start.id('start');
-  newEQ.id('start');
+function setupSelector() {
+
+    var label = createElement('label', "Select a difficulty of problem");
+    label.parent(slate);
+    label.attribute('for', 'difficulty')
+    label.id('label');
+    $('#label').click(function() {
+        $(this).siblings('select').css('width', $(this).outerWidth(true)).toggle();
+    });
+    createElement('br').attribute('clear', 'all').parent(slate);
+    difficulty = createSelect();
+    difficulty.id('dif');
+    difficulty.attribute('name', 'difficulty');
+    difficulty.attribute('style', 'display:none;');
+    difficulty.parent(slate);
+
+    //----------------------------------------------------
+    $.getJSON("problems.json",
+        function(json) {
+          problemSet = json;
+          console.log(json);
+            console.log("JSON LOADED");
+            for (var i = 0; i < problemSet.problem.length; i++) {
+                console.log("GOing through the problemSet " + problemSet.problem[i].difficulty);
+                difficulty.option(problemSet.problem[i].difficulty);
+            }
+        }
+    );
+}
+
+function setupEquation() {
+    // var s = "\\frac{1+sin(x)}{y}";
+    var equation = createImg(callLink + "\e^{i\\pi}+1");
+    equation.parent(slate);
+    $('#slate').children("img").hide();
+    $('#slate').children("img").fadeIn();
+}
+
+function makeButton() {
+    var buttonDiv = createDiv("");
+    start = createButton("Start Timer");
+    var newEQ = createButton("New Equation");
+    start.parent(buttonDiv);
+    newEQ.parent(buttonDiv);
+    buttonDiv.id("butDiv");
+    start.id('start');
+    newEQ.id('start');
 }
